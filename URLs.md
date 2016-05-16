134dbb2e780184dfa1f1a22a90e47c4db06701ae
Gilmore Davidson <gilmoreorless@gmail.com>
2016-05-16 22:35:59 +1000

    Added stub pages with routing - closes #2

---
+++ URLs.md
@@ -0,0 +1,9 @@
+# URL structure
+
+* `/` — index / home
+* `/how-it-works` — index page for "How it works" section
+  + `/house-of-reps` — explanation for House of Reps
+  + `/senate` — explanation for Senate
+  + `/prime-minister` — explanation for PM
+* `/results` — index page for actual results
+  + ??? (TBC)
--- server.js
+++ server.js
@@ -3,10 +3,35 @@ var express = require('express');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-app.get('/', function (req, res) {
-    res.json({ok: true});
-});
+function dumbRender(req, res) {
+    var path = req.path.substr(1);
+    res.render(path);
+}
+
+function setupDumbPaths(paths, prefix = '') {
+    var prevPath = prefix;
+    paths.forEach(function (path) {
+        if (Array.isArray(path)) {
+            setupDumbPaths(path, prevPath);
+        } else {
+            let fullPath = prefix + '/' + path;
+            app.get(fullPath, dumbRender);
+            prevPath = fullPath;
+        }
+    });
+}
+
+setupDumbPaths([
+    '',
+    'how-it-works', [
+        'house-of-reps',
+        'senate',
+        'prime-minister',
+    ],
+    'results'
+]);
 
 app.listen(parseInt(process.env.PORT || 9876, 10));
